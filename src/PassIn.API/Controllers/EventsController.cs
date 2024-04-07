226539c8e884c7d29f5b464f1f0a7119f95017ae
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-07 12:00:24 -0300

    feat(api): add events controller

---
+++ src/PassIn.API/Controllers/EventsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Communication.Requests;
+
+namespace PassIn.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestEventJson request)
+    {
+        return Created();
+    }
+}
