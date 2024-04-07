9d826014ac1812e4b3f7fd01d408c194eb1f5596
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-07 13:49:56 -0300

    feat: add database interaction
    Add sqlite database.
    Add entity framework and sqlite packages to Infrastructure.
    Add dbContext.
    Add event entity.

---
+++ src/PassIn.Infrastructure/PassInDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Infrastructure.Entities;
+
+namespace PassIn.Infrastructure;
+public class PassInDbContext : DbContext
+{
+    public DbSet<Event> Events { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data_Source=./Database/PassInDb.db");
+    }
+}
