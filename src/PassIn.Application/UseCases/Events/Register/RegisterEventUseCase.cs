14029e9f946dcd135bb11c020bd383dff87165e3
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-08 10:40:27 -0300

    feat(application): update register event use case
    Now it will save to the database

--- src/PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
+++ src/PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using PassIn.Communication.Requests;
 using PassIn.Exceptions;
+using PassIn.Infrastructure;
 
 namespace PassIn.Application.UseCases.Events.Register;
 public class RegisterEventUseCase
@@ -7,6 +8,18 @@ public class RegisterEventUseCase
     public void Execute(RequestEventJson request)
     {
         Validate(request);
+
+        var dbContext = new PassInDbContext();
+        var entity = new Infrastructure.Entities.Event
+        {
+            Title = request.Title,
+            Details = request.Details,
+            Maximum_Attendees = request.MaximumAttendees,
+            Slug = request.Title.ToLower().Replace(" ", "-")
+        };
+
+        dbContext.Events.Add(entity);
+        dbContext.SaveChanges();
     }
 
     private void Validate(RequestEventJson request)
