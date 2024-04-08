5311834b32c1c0946464d22eab293005858899a8
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-08 11:19:22 -0300

    feat(application): add get event by id use case

---
+++ src/PassIn.Application/UseCases/Events/GetById/GetEventByIdUseCase.cs
@@ -0,0 +1,25 @@
+﻿using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+
+namespace PassIn.Application.UseCases.Events.GetById;
+public class GetEventByIdUseCase
+{
+    public ResponseEventJson Execute(Guid eventId)
+    {
+        var dbContext = new PassInDbContext();
+        var entity = dbContext.Events.FirstOrDefault(ev => ev.Id == eventId);
+
+        if (entity is null)
+            throw new PassInException("Event not found with this id.");
+
+        return new ResponseEventJson
+        {
+            Id = entity.Id,
+            Title = entity.Title,
+            Details = entity.Details,
+            MaximumAttendees = entity.Maximum_Attendees,
+            AttendeesAmount = -1
+        };
+    }
+}
