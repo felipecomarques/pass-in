d7d0aa00de1f5d0f79cb736196285fee572bb217
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-09 13:23:15 -0300

    refactor: add attendees controller

---
+++ src/PassIn.API/Controllers/AttendeesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.RegisterAttendee;
+using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+
+namespace PassIn.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AttendeesController : ControllerBase
+{
+    [HttpPost]
+    [Route("{eventId}/Register")]
+    [ProducesResponseType(typeof(ResponseRegisteredJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status409Conflict)]
+    public IActionResult RegisterAttendee([FromRoute] Guid eventId, [FromBody] RequestRegisterEventJson request)
+    {
+        var useCase = new RegisterAttendeeOnEventUseCase();
+        var response = useCase.Execute(eventId, request);
+        return Created(string.Empty, response);
+    }
+}
--- src/PassIn.API/Controllers/EventsController.cs
+++ src/PassIn.API/Controllers/EventsController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PassIn.Application.UseCases.Events.GetById;
 using PassIn.Application.UseCases.Events.Register;
-using PassIn.Application.UseCases.Events.RegisterAttendee;
 using PassIn.Communication.Requests;
 using PassIn.Communication.Responses;
 
@@ -28,19 +27,6 @@ public class EventsController : ControllerBase
     {
         var useCase = new RegisterEventUseCase();
         var response = useCase.Execute(request);
-        return Created(string.Empty, response);
-    }
-
-    [HttpPost]
-    [Route("{eventId}/Register")]
-    [ProducesResponseType(typeof(ResponseRegisteredJson), StatusCodes.Status201Created)]
-    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
-    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status409Conflict)]
-    public IActionResult RegisterAttendee([FromRoute] Guid eventId, [FromBody] RequestRegisterEventJson request)
-    {
-        var useCase = new RegisterAttendeeOnEventUseCase();
-        var response = useCase.Execute(eventId, request);
         return Created(string.Empty, response);
     }
 }
