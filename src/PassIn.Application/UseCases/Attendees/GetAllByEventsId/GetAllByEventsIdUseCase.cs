d6c876cc3738f44c8cfcf181b8f47748d5e18f42
Felipe Marques <felipe.comarques1208@gmail.com>
2024-04-09 14:27:56 -0300

    feat(application): add get all attendees use case

---
+++ src/PassIn.Application/UseCases/Attendees/GetAllByEventsId/GetAllByEventsIdUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+
+namespace PassIn.Application.UseCases.Attendees.GetAllByEventsId;
+public class GetAllByEventsIdUseCase
+{
+    private readonly PassInDbContext _dbContext;
+
+    public GetAllByEventsIdUseCase()
+    {
+        _dbContext = new PassInDbContext();
+    }
+    public ResponseAllAttendeesJson Execute(Guid eventId)
+    {
+        //var attendees = _dbContext.Attendees.Where(attendee => attendee.Event_Id == eventId).ToList();
+        var attendees = _dbContext.Events.Include(ev => ev.Attendees).FirstOrDefault(ev => ev.Id == eventId);
+        if (attendees is null)
+            throw new NotFoundException("Attendees not found with this event id.");
+        return new ResponseAllAttendeesJson
+        {
+            Attendees = attendees.Attendees.Select(attendee => new ResponseAttendeeJson
+            {
+                Id = attendee.Id,
+                Name = attendee.Name,
+                Email = attendee.Email,
+                CreatedAt = attendee.Created_At
+            }).ToList()
+        };
+    }
+}
